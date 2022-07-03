95275bfd0a9728becf52730e358d69cd21f84dc7
cool29 <cool29@yandex.ru>
2022-07-03 10:30:12 +0500

    add readme and about page

---
+++ README.md
@@ -0,0 +1,24 @@
+#Тестовое задание
+
+Это тестовое задание сделанное для Вашей компании.
+Все пункты задания выполнены успешно.
+
+
+#зависимости
+node 14, yarn
+
+#Стек
+
+typescript, react, redux-toolkit, date-fns, styled-components, docker, eslint, prettier
+
+#Запуск
+
+В папке с проектом запустить
+
+> yarn
+>
+> yarn start
+
+или
+
+> npm i && npm run start
--- src/features/About/About.tsx
+++ src/features/About/About.tsx
@@ -2,7 +2,66 @@ import { FC } from 'react';
 import styled from 'styled-components';
 
 export const About: FC = () => {
-  return <Wrap>About</Wrap>;
+  return (
+    <SWrap>
+      <SBlock>
+        <h3>О проекте</h3>
+        <p>
+          Данный проект предназначен для демонстрации навыков разработки на
+          react и сопутствующих технологиях
+        </p>
+      </SBlock>
+
+      <SBlock>
+        <h3>ЗАМЕЧАНИЯ ПО ДОБАВЛЕНИЮ НОВОГО СОБЫТИЯ</h3>
+        <p>
+          ЕСЛИ В ТЕЧЕНИИ ЧАСА, НА КТОРЫЙ ВЫ ДОБАВЛЯЕТЕ СОБЫТИЕ, УЖЕ ЕСТЬ ЕЩЕ
+          ОДНО СОБЫТИЕ, ТО ВРЕМЯ БУДЕТ СЧИТАТЬСЯ ЗАНЯТЫМ И ВЫ ПОЛУЧИТЕ ОШИБКУ
+          {' "'}TIME IS BUSY{'"'}
+        </p>
+      </SBlock>
+
+      <SBlock>
+        <h3>Стек</h3>
+        <p>
+          typescript, react, redux-toolkit, date-fns, styled-components, docker,
+          eslint, prettier
+        </p>
+      </SBlock>
+
+      <SBlock>
+        <h3>Сведения о разработчике</h3>
+        <p>
+          Меня зовут Василий. Я разработчик js. Специализируюсь на стеке
+          typescript, react, redux-toolkit и фронтент разработке, но не
+          ограничен данными технологиями ))
+        </p>
+      </SBlock>
+
+      <SBlock>
+        <h3>github</h3>
+        <a href="https://github.com/horhoj">https://github.com/horhoj</a>
+      </SBlock>
+
+      <SBlock>
+        <h3>Telegram</h3>
+        <a href="https://t.me/sarevok_horhoj">https://t.me/sarevok_horhoj</a>
+      </SBlock>
+    </SWrap>
+  );
 };
 
-const Wrap = styled.div``;
+const SWrap = styled.div`
+  padding: 10px;
+  margin: 0 auto;
+  max-width: 740px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const SBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+`;
